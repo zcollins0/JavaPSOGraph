5b70a08c32a192244156dc08a7056fd3b07b7ffb
polekas55 <polekas55@gmail.com>
2015-10-24 13:01:12 -0400

    commit

--- JavaPSOGraph/psograph/AnalyzeGraph.java
+++ JavaPSOGraph/psograph/AnalyzeGraph.java
@@ -16,7 +16,6 @@
 //   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 package psograph;
 
-import psograph.analysis.BasicDataPlots;
 import psograph.analysis.CreateFitnessCompHistograms;
 import psograph.analysis.CreateHistogram;
 import psograph.analysis.FitnessFunctionGraphs;
