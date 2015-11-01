e73f11640e5b12c78f6d8b427fad18cf3dd0faa0
polekas55 <polekas55@gmail.com>
2015-11-01 13:33:52 -0500

    Add edge unit test

---
+++ JavaPSOGraph/psograph/graph/EdgeTest.java
@@ -0,0 +1,39 @@
+package psograph.graph;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class EdgeTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testEdgeEdge() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEdgeDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testModifyWeight() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetWeight() {
+		fail("Not yet implemented");
+	}
+
+}
