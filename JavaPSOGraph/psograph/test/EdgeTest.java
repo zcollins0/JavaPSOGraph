4a9c7307aa45dea445182030cb86eca02fe53420
polekas55 <polekas55@gmail.com>
2015-11-04 21:00:53 -0500

    Implement and Move EdgeTest

---
+++ JavaPSOGraph/psograph/test/EdgeTest.java
@@ -0,0 +1,49 @@
+package psograph.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import psograph.graph.Edge;
+
+public class EdgeTest {
+
+
+
+	@Test
+	public void testEdgeEdge() 
+	{
+		Edge e = new Edge(.55);
+		Edge e2 = new Edge(e);
+		
+		assertTrue(Double.compare(e2.getWeight(),.55) == 0);
+	}
+
+	@Test
+	public void testEdgeDouble() 
+	{
+		Edge e = new Edge(.55);
+		
+		assertTrue(Double.compare(e.getWeight(),.55) == 0);
+	}
+
+	@Test
+	public void testModifyWeight() 
+	{
+		Edge e = new Edge(.55);
+		
+		assertTrue(Double.compare(e.getWeight(),.55) == 0);
+		
+		e.modifyWeight(.76);
+		
+		assertTrue(Double.compare(e.getWeight(),.76) == 0);
+	}
+
+//	@Test
+//	public void testGetWeight() {
+//		fail("Not yet implemented");
+//	}
+
+}
